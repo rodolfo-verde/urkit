f91dc63e0db1b5eaad651b1b9379385c2bfbf9b9
rodolfo <rodolfo.f.verde@gmail.com>
2026-07-03 15:49:52 +0200

    bump version to 0.3.4
    - Reachability pre-check for Cartesian moves

--- src/urkit/__init__.py
+++ src/urkit/__init__.py
@@ -24,7 +24,7 @@ Quick start::
 
 from __future__ import annotations
 
-__version__ = "0.3.3"
+__version__ = "0.3.4"
 
 from urkit.config import load_config, resolve_config
 from urkit.exceptions import (
